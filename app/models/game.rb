d5b7f7d93e40b36aa78e96510797da364026af0b
kawabata <s1931115@s.konan-u.ac.jp>
2022-04-17 14:18:56 +0900

    [update]progress model

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,5 +4,5 @@ class Game < ApplicationRecord
   enum status: { finish: 0, in_progress: 1 }
   enum result: { correct: 0, in_correct: 1 }
   validates :status, inclusion: { in: Game.statuses.keys }
-  validates :result, inclusion: { in: Game.results.keys }
+  # validates :result, inclusion: { in: Game.results.keys }
 end
--- app/models/progress.rb
+++ app/models/progress.rb
@@ -5,4 +5,17 @@ class Progress < ApplicationRecord
   enum answer: { positive: 0, negative: 1, unknown: 2 }
 
   validates :answer, inclusion: { in: Progress.answers.keys }
+
+  def assign_sequence
+    next_sequence = 1
+    # game_idが存在している時のみ処理を実行する
+    #
+    if game.present?
+      all_progress = game.progresses
+      if all_progress.count > 0
+        next_sequence = all_progress.length
+      end
+    end
+    self.sequence = next_sequence
+  end
 end
